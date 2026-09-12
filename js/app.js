df88af6d485ad1be60c2212b99e388702776480a
BoredVentura <worldofpowers.hq@gmail.com>
2026-09-12 17:20:53 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -403,6 +403,29 @@ const progressValue = document.getElementById("progressValue");
 
     });
 
+document
+  .querySelectorAll(".update-goal")
+  .forEach(function (button) {
+
+    button.addEventListener("click", function () {
+
+      const id = Number(this.dataset.id);
+
+      const goal = goals.find(function (goal) {
+        return goal.id === id;
+      });
+
+      if (!goal) return;
+
+      progressGoalId.value = goal.id;
+      progressValue.value = goal.progress || 0;
+
+      progressModal.classList.add("is-open");
+
+    });
+
+  });
+    
   updateSummary();
   }
 
